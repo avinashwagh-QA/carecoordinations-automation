fa555a12ad6322e99ed02f3689c4a0ef8dd6f6df
avinashwagh-QA <avinashwagh@nexios.in>
2026-01-13 18:19:13 +0530

    Fet: Implementing Reset password Page

---
+++ src/main/java/com/carecoordination/healthcare/pages/landingPages/ResetPasswordPage.java
@@ -0,0 +1,70 @@
+package com.carecoordination.healthcare.pages.landingPages;
+
+import com.carecoordination.healthcare.actiondriver.ActionDriver;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+
+public class ResetPasswordPage {
+
+    private ActionDriver actionDriver;
+
+    private static Logger logger = LogManager.getLogger(ResetPasswordPage.class);
+
+    //Initialize the action drive object by passing the webdriver instance
+    public ResetPasswordPage(ActionDriver actionDriver){
+        this.actionDriver = actionDriver;
+    }
+
+    //Defining locators for ForgotPassword
+    private final By titleResetPassword = By.xpath("//h6[normalize-space()='Reset password']");
+
+    private final By subTitleResetPassword = By.xpath("//p[normalize-space()='Please enter a new password']");
+
+    private final By inputNewPassword = By.id("password_reset");
+    private final By inputConfirmPassword = By.id("confirm_password_reset");
+
+    private final By btnSubmit= By.id("resetPasswordBtn");
+
+    //Title for reset password
+    public String getResetPasswordPageTitle(){
+        actionDriver.waitForElementToVisible(titleResetPassword);
+        String title = actionDriver.getText(titleResetPassword);
+        logger.info("Title displayed on reset password is {}", title);
+        return title;
+    }
+
+    //Subtitle is displayed for reset password
+    public String getSubtitleResetPassword(){
+        actionDriver.waitForElementToVisible(subTitleResetPassword);
+        return actionDriver.getText(subTitleResetPassword);
+    }
+
+    public boolean isNewPasswordAndConfirmPasswordDisplayed(){
+        actionDriver.waitForElementToVisible(inputNewPassword);
+        actionDriver.waitForElementToVisible(inputConfirmPassword);
+        boolean password = actionDriver.isDisplayed(inputNewPassword) && actionDriver.isDisplayed(getInputConfirmPassword);
+        logger.info("On reset password page new password and confirm password field is displayed...");
+        return password;
+    }
+
+    public void setInputNewPassword(String newPassword){
+        actionDriver.waitForElementToBeClickable(inputNewPassword);
+        actionDriver.enterText(inputNewPassword, newPassword);
+    }
+
+    public void setInputConfirmPassword(String confirmPassword){
+        actionDriver.waitForElementToBeClickable(inputConfirmPassword);
+        actionDriver.enterText(inputNewPassword, confirmPassword);
+    }
+
+    public boolean isButtonDisplayed(){
+
+        actionDriver.waitForAllElementsToBeVisible(btnSubmit);
+        actionDriver
+
+    }
+
+
+
+}
