9f602bea3c60889951fa17e0e9be01c8e1c3c904
avinashwagh-QA <avinashwagh@nexios.in>
2026-03-05 16:12:45 +0530

    Component added for branch selection and Table WIP : Manage team Invite flow

---
+++ src/main/java/com/carecoordination/healthcare/components/TableComponent.java
@@ -0,0 +1,171 @@
+package com.carecoordination.healthcare.components;
+
+import com.carecoordination.healthcare.actiondriver.ActionDriver;
+import com.carecoordination.healthcare.factory.DriverFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TableComponent {
+
+   private final WebDriver driver;
+   private final ActionDriver actionDriver;
+
+   private final By rowLocator;
+   private final By headerLocator;
+
+   private Map<String, Integer> columnMap;
+    private static final Logger logger = LogManager.getLogger(TableComponent.class);
+
+    public TableComponent(By headerLocator, By rowLocator){
+        this.driver = DriverFactory.getDriver();
+        this.actionDriver = new ActionDriver(driver);
+
+        this.rowLocator = rowLocator;
+        this.headerLocator = headerLocator;
+
+        buildColumnMap();
+    }
+
+    //Build Header Map
+    private void buildColumnMap(){
+
+        actionDriver.waitForElementToVisible(headerLocator);
+        columnMap = new HashMap<>();
+
+        List<WebElement> headers = driver.findElements(headerLocator);
+
+        for (int i=0;i<headers.size(); i++){
+
+            String headerText = headers.get(i).getText().trim();
+            columnMap.put(headerText, i+1);
+        }
+        logger.info("Table column map created: {}", columnMap);
+    }
+
+    // Validate column
+    private int getColumnIndex(String columnName) {
+
+        if (!columnMap.containsKey(columnName)) {
+
+            throw new RuntimeException(
+                    "Column not found: " + columnName +
+                            ". Available columns: " + columnMap.keySet()
+            );
+        }
+
+        return columnMap.get(columnName);
+    }
+
+    //Get All rows
+    public List<WebElement> getRows(){
+        actionDriver.waitForElementToVisible(rowLocator);
+        List<WebElement> rows =driver.findElements(rowLocator);
+
+        logger.info("Total rows found  in table{}", rows.size());
+        return rows;
+    }
+
+    //Get rows count
+    public int getRowCount(){
+        int count = getRows().size();
+        logger.info("Row count: {}", count);
+        return count;
+    }
+
+    //Find row by column value
+    public WebElement findRow(String columnName, String value) {
+
+        int columnIndex = columnMap.get(columnName);
+
+        for (WebElement row : getRows()) {
+
+            String cellText = row.findElement(By.xpath("./div[" + columnIndex + "]"))
+                    .getText().trim();
+
+            if (cellText.equalsIgnoreCase(value)) {
+
+                logger.info("Row found where {} = {}", columnName, value);
+                return row;
+            }
+        }
+
+        logger.warn("Row not found where {} = {}", columnName, value);
+        return null;
+    }
+
+    // Get cell text
+    public String getCellText(String searchColumn, String searchValue, String targetColumn) {
+
+        WebElement row = findRow(searchColumn, searchValue);
+
+        if (row == null) {
+            throw new RuntimeException(
+                    "Row not found where " + searchColumn + " = " + searchValue
+            );
+        }
+        int tagetIndex = getColumnIndex(targetColumn);
+
+        String cellText = row.findElement(By.xpath("./div[" + tagetIndex + "]"))
+                .getText().trim();
+
+        logger.info("Cell value for {} -> {} : {}",searchColumn,searchValue,cellText);
+        return cellText;
+    }
+
+    //Click action inside row
+    public void clickCell(String searchColumn, String searchValue, String targetColumn){
+
+        WebElement row = findRow(searchColumn, searchValue);
+        if (row == null) {
+
+            throw new RuntimeException(
+                    "Row not found where " + searchColumn + " = " + searchValue
+            );
+        }
+
+        int columnIndex = columnMap.get(targetColumn);
+
+        WebElement cell = row.findElement(By.xpath("./div[" + columnIndex + "]"));
+
+        actionDriver.click(cell);
+        logger.info("Clicked column '{}' in row where {} = {}",targetColumn,searchColumn,searchValue);
+    }
+
+    // Verify row exists
+    public boolean isRowPresent(String columnName,String value){
+        return findRow(columnName,value)!=null;
+    }
+
+    public void clickOnAction(String searchColumn, String searchValue, String actionName){
+
+        WebElement row = findRow(searchColumn, searchValue);
+
+        if (row == null) {
+
+            throw new RuntimeException(
+                    "Row not found where " + searchColumn + " = " + searchValue
+            );
+        }
+
+        int actionColumn = columnMap.get("Actions");
+
+        // Click action button
+        WebElement action = row.findElement(By.xpath("./div[" + actionColumn + "]"));
+        actionDriver.click(action);
+
+        //click on action menu
+        By actionOption =  By.xpath("//button[normalize-space()='"+actionName+"']");
+        actionDriver.click(actionOption);
+
+        logger.info("Performed '{}' action on row where {} = {}",
+                actionName, searchColumn, searchValue);
+    }
+
+}
--- src/main/java/com/carecoordination/healthcare/pages/modules/AppDashBoard/ManageTeam/ManageTeamPage.java
+++ src/main/java/com/carecoordination/healthcare/pages/modules/AppDashBoard/ManageTeam/ManageTeamPage.java
@@ -1,34 +1,41 @@
 package com.carecoordination.healthcare.pages.modules.AppDashBoard.ManageTeam;
 
 import com.carecoordination.healthcare.actiondriver.ActionDriver;
+import com.carecoordination.healthcare.components.BranchSelectorComponent;
+import com.carecoordination.healthcare.factory.DriverFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class ManageTeamPage {
 
     private final ActionDriver actionDriver;
 
+
+    private final BranchSelectorComponent branchSelectorComponent;
+
     private static final Logger logger = LogManager.getLogger(ManageTeamPage.class);
 
     public ManageTeamPage(ActionDriver actionDriver) {
         this.actionDriver = actionDriver;
+        this.branchSelectorComponent = new BranchSelectorComponent();
     }
 
     //Locators for ManageTeam
     private final By tabManageTeam = By.xpath("//a[contains(@href,'manage-team-list') and normalize-space()='Manage Team']");
     private final By tabManageMiniTeams = By.xpath("//a[contains(@href,'manage-mini-team') and normalize-space()='Manage Mini-Teams']");
 
-    private final By drpBranchSelection = By.id("selectBranchSectionTeam");
-
     private final By btnInviteUser = By.id("createInviteUser");
     private final By inpSearchUser = By.id("searchUsersInput");
-
 
     private final By activeTab = By.id("activeTab");
     private final By pendingTab = By.id("PendingRegTab");
     private final By teamHistory = By.id("TeamHistoryTab");
 
+    private final By pagination = By.xpath("//div[@id='manageteamListingsTbl_paginate']//a[contains(@class,'paginate_button')]");
 
     public void clickOnManageTeamTab(){
         actionDriver.waitForElementToBeClickable(tabManageTeam);
@@ -60,7 +67,36 @@ public class ManageTeamPage {
         logger.info("Click on Team History tab - Mange team in Team menu....");
     }
 
+    public void selectBranch(String branchName){
+        branchSelectorComponent.selectBranch(branchName);
+    }
 
+    public void searchManageTeamByStaffName(String staffName){
+        actionDriver.waitForElementToBeClickable(inpSearchUser);
+        actionDriver.enterText(inpSearchUser, staffName);
+        logger.info("Search on Manage team by Staff name : {}", staffName);
+    }
+
+    public void SearchManageTeamByEmail(String email){
+        actionDriver.waitForElementToBeClickable(inpSearchUser);
+        actionDriver.enterText(inpSearchUser, email);
+    }
+
+
+
+    //Pagination method to  get all pages
+    public int getAllPages(){
+        actionDriver.waitForAllElementsToBeVisible(pagination);
+        List<WebElement> pages = DriverFactory.getDriver().findElements(pagination);
+        return  pages.size();
+    }
+
+    //Click on Page number
+    public void clickOnPageNumber(int pageNumber) {
+        By page = By.xpath("//div[@id='manageteamListingsTbl_paginate']//a[normalize-space()='" + pageNumber + "']");
+        actionDriver.click(page);
+        logger.info("Click on page number :{}", pageNumber);
+    }
 
 
 }
